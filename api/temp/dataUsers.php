3121d77dc9dae09a57941d9041e61b3b51e23966
muhammadamran <amranhakim9@gmail.com>
2022-10-15 20:54:14 +0700

    Intial commit

--- api/temp/dataUsers.php
+++ api/temp/dataUsers.php
@@ -1,13 +1,16 @@
 <?php
-$api_url = 'http://127.0.0.1/tpbbackend/api/apidataUsers.php?id_user';
+$api_url = 'https://itinventory-sarinah.com/test_api_data.php';
 $content = file_get_contents($api_url);
 $data = json_decode($content, true);
+
+var_dump($data);
+exit;
 
 $response = $data['status'];
 echo $response;
 echo "<br>";
 
 foreach ($data['result'] as $row) {
-    echo $row['id'];
+    echo $row['ID'];
     echo "<br>";
 }
