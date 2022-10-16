46222b4b5bb4208522ae3ef2e116a27e71ce47ff
muhammadamran <amranhakim9@gmail.com>
2022-10-16 11:34:52 +0700

    Initial commit

--- api/viewDODashboard.php
+++ api/viewDODashboard.php
@@ -5,12 +5,46 @@ if (function_exists($_GET['function'])) {
     $_GET['function']();
 }
 
+// BC TPB
+function get_bcTPB()
+{
+    global $db;
+    $dataGetBC = $db->query("SELECT NOMOR_AJU,KODE_DOKUMEN_PABEAN,
+                            (SELECT COUNT(*) AS total_bc FROM tpb_header WHERE KODE_DOKUMEN_PABEAN IS NOT NULL) AS total_bc
+                            FROM tpb_header
+                            WHERE KODE_DOKUMEN_PABEAN IS NOT NULL
+                            ORDER BY ID DESC LIMIT 1", 0);
+    $cek = $dataGetBC->num_rows;
+
+    if ($cek > 0) {
+        $data = [];
+
+        while ($result = $dataGetBC->fetch_assoc()) {
+            $data[] = [
+                'NOMOR_AJU' => $result['NOMOR_AJU'],
+                'KODE_DOKUMEN_PABEAN' => $result['KODE_DOKUMEN_PABEAN'],
+                'total_bc' => $result['total_bc']
+            ];
+        }
+
+        echo json_encode([
+            'status' => 200,
+            'result' => $data
+        ]);
+    } else {
+        echo json_encode([
+            'status' => 404,
+            'result' => 'Data not found'
+        ]);
+    }
+}
+
 // BC 2.3
 function get_bc23()
 {
     global $db;
     $dataGetBC23 = $db->query("SELECT NOMOR_AJU,
-                            (SELECT COUNT(*) AS total_bc23 FROM tpb_header WHERE KODE_DOKUMEN_PABEAN=23) AS total_bc23
+                            (SELECT COUNT(*) AS total_bc23 FROM tpb_header WHERE KODE_DOKUMEN_PABEAN=23 AND KODE_DOKUMEN_PABEAN IS NOT NULL) AS total_bc23
                             FROM tpb_header
                             WHERE KODE_DOKUMEN_PABEAN=23
                             AND KODE_DOKUMEN_PABEAN IS NOT NULL
@@ -44,7 +78,7 @@ function get_bc25()
 {
     global $db;
     $dataGetBC25 = $db->query("SELECT NOMOR_AJU,
-                            (SELECT COUNT(*) AS total_bc25 FROM tpb_header WHERE KODE_DOKUMEN_PABEAN=25) AS total_bc25
+                            (SELECT COUNT(*) AS total_bc25 FROM tpb_header WHERE KODE_DOKUMEN_PABEAN=25 AND KODE_DOKUMEN_PABEAN IS NOT NULL) AS total_bc25
                             FROM tpb_header
                             WHERE KODE_DOKUMEN_PABEAN=25
                             AND KODE_DOKUMEN_PABEAN IS NOT NULL
@@ -78,7 +112,7 @@ function get_bc261()
 {
     global $db;
     $dataGetBC261 = $db->query("SELECT NOMOR_AJU,
-                            (SELECT COUNT(*) AS total_bc261 FROM tpb_header WHERE KODE_DOKUMEN_PABEAN=261) AS total_bc261
+                            (SELECT COUNT(*) AS total_bc261 FROM tpb_header WHERE KODE_DOKUMEN_PABEAN=261 AND KODE_DOKUMEN_PABEAN IS NOT NULL) AS total_bc261
                             FROM tpb_header
                             WHERE KODE_DOKUMEN_PABEAN=261
                             AND KODE_DOKUMEN_PABEAN IS NOT NULL
@@ -112,7 +146,7 @@ function get_bc27()
 {
     global $db;
     $dataGetBC27 = $db->query("SELECT NOMOR_AJU,
-                            (SELECT COUNT(*) AS total_bc27 FROM tpb_header WHERE KODE_DOKUMEN_PABEAN=27) AS total_bc27
+                            (SELECT COUNT(*) AS total_bc27 FROM tpb_header WHERE KODE_DOKUMEN_PABEAN=27 AND KODE_DOKUMEN_PABEAN IS NOT NULL) AS total_bc27
                             FROM tpb_header
                             WHERE KODE_DOKUMEN_PABEAN=27
                             AND KODE_DOKUMEN_PABEAN IS NOT NULL
@@ -146,7 +180,7 @@ function get_bc40()
 {
     global $db;
     $dataGetBC40 = $db->query("SELECT NOMOR_AJU,
-                            (SELECT COUNT(*) AS total_bc40 FROM tpb_header WHERE KODE_DOKUMEN_PABEAN=40) AS total_bc40
+                            (SELECT COUNT(*) AS total_bc40 FROM tpb_header WHERE KODE_DOKUMEN_PABEAN=40 AND KODE_DOKUMEN_PABEAN IS NOT NULL) AS total_bc40
                             FROM tpb_header
                             WHERE KODE_DOKUMEN_PABEAN=40
                             AND KODE_DOKUMEN_PABEAN IS NOT NULL
@@ -180,7 +214,7 @@ function get_bc41()
 {
     global $db;
     $dataGetBC41 = $db->query("SELECT NOMOR_AJU,
-                            (SELECT COUNT(*) AS total_bc41 FROM tpb_header WHERE KODE_DOKUMEN_PABEAN=41) AS total_bc41
+                            (SELECT COUNT(*) AS total_bc41 FROM tpb_header WHERE KODE_DOKUMEN_PABEAN=41 AND KODE_DOKUMEN_PABEAN IS NOT NULL) AS total_bc41
                             FROM tpb_header
                             WHERE KODE_DOKUMEN_PABEAN=41
                             AND KODE_DOKUMEN_PABEAN IS NOT NULL
