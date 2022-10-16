e6cc1950432169daa4b3f9115191937f8a06ee61
muhammadamran <amranhakim9@gmail.com>
2022-10-17 02:25:55 +0700

    Initial commit

--- api/reportKeluarBarang.php
+++ api/reportKeluarBarang.php
@@ -3,19 +3,19 @@ include "db.php";
 
 if ($_GET["StartTanggal"] == 0 || $_GET["StartTanggal"] == '' || $_GET["StartTanggal"] == NULL) {
     $dataGet = $db->query("SELECT plb.NOMOR_BC11 AS PLB_NOMOR_BC11,plb.TANGGAL_BC11 AS PLB_TANGGAL_BC11,hdr.NOMOR_BC11,hdr.TANGGAL_BC11,hdr.NAMA_PEMASOK,
-                                    brg.KODE_BARANG,brg.URAIAN,brg.KODE_SATUAN,brg.JUMLAH_SATUAN,hdr.KODE_VALUTA,brg.CIF,hdr.ID_PENERIMA_BARANG
+                                    brg.KODE_BARANG,brg.URAIAN,brg.KODE_SATUAN,brg.JUMLAH_SATUAN,hdr.KODE_VALUTA,brg.CIF,hdr.ID_PENERIMA_BARANG,hdr.KODE_DOKUMEN_PABEAN,hdr.TANGGAL_TTD,SUBSTR(hdr.NOMOR_AJU,13,8) AS TGL_AJU,hdr.NOMOR_AJU
                             FROM tpb_header AS hdr
                             LEFT JOIN plb_header AS plb ON hdr.NOMOR_DAFTAR=plb.NOMOR_DAFTAR
                             LEFT OUTER JOIN tpb_barang AS brg ON hdr.ID=brg.ID_HEADER
                             ORDER BY hdr.TANGGAL_BC11 ASC LIMIT 0", 0);
 } else {
     $dataGet = $db->query("SELECT plb.NOMOR_BC11 AS PLB_NOMOR_BC11,plb.TANGGAL_BC11 AS PLB_TANGGAL_BC11,hdr.NOMOR_BC11,hdr.TANGGAL_BC11,hdr.NAMA_PEMASOK,
-                            brg.KODE_BARANG,brg.URAIAN,brg.KODE_SATUAN,brg.JUMLAH_SATUAN,hdr.KODE_VALUTA,brg.CIF,hdr.ID_PENERIMA_BARANG
+                            brg.KODE_BARANG,brg.URAIAN,brg.KODE_SATUAN,brg.JUMLAH_SATUAN,hdr.KODE_VALUTA,brg.CIF,hdr.ID_PENERIMA_BARANG,hdr.KODE_DOKUMEN_PABEAN,hdr.TANGGAL_TTD,SUBSTR(hdr.NOMOR_AJU,13,8) AS TGL_AJU,hdr.NOMOR_AJU
                             FROM tpb_header AS hdr
                             LEFT JOIN plb_header AS plb ON hdr.NOMOR_DAFTAR=plb.NOMOR_DAFTAR
                             LEFT OUTER JOIN tpb_barang AS brg ON hdr.ID=brg.ID_HEADER
-                            WHERE hdr.TANGGAL_BC11 BETWEEN " . $_GET['StartTanggal'] . " AND " . $_GET['EndTanggal'] . "
-                            ORDER BY hdr.TANGGAL_BC11,brg.KODE_BARANG,brg.URAIAN ASC", 0);
+                            WHERE SUBSTR(hdr.NOMOR_AJU,13,8) BETWEEN " . $_GET['StartTanggal'] . " AND " . $_GET['EndTanggal'] . "
+                            ORDER BY SUBSTR(hdr.NOMOR_AJU,13,8) ASC", 0);
 }
 $cek = $dataGet->num_rows;
 
@@ -24,9 +24,17 @@ if ($cek > 0) {
 
     while ($result = $dataGet->fetch_assoc()) {
         $data[] = [
+            'KODE_SATUAN' => $result['KODE_SATUAN'],
+            'JUMLAH_SATUAN' => $result['JUMLAH_SATUAN'],
+            'KODE_VALUTA' => $result['KODE_VALUTA'],
+            'CIF' => $result['CIF'],
+            'NOMOR_AJU' => $result['NOMOR_AJU'],
+            'TGL_AJU' => $result['TGL_AJU'],
+            'TANGGAL_TTD' => $result['TANGGAL_TTD'],
+            'KODE_DOKUMEN_PABEAN' => $result['KODE_DOKUMEN_PABEAN'],
             'NOMOR_BC11' => $result['NOMOR_BC11'],
             'TANGGAL_BC11' => $result['TANGGAL_BC11'],
-            'PEMASOK' => $result['PEMASOK'],
+            'NAMA_PEMASOK' => $result['NAMA_PEMASOK'],
             'KODE_BARANG' => $result['KODE_BARANG'],
             'URAIAN' => preg_replace('/[^a-zA-Z0-9]/', ' ', $result['URAIAN'])
         ];
