fa311723f8e2788d626025929ef7c275a85da53f
muhammadamran <amranhakim9@gmail.com>
2022-10-19 17:48:09 +0700

    Intial commit

--- api/reportKeluarBarang.php
+++ api/reportKeluarBarang.php
@@ -7,14 +7,15 @@ if ($_GET["StartTanggal"] == 0 || $_GET["StartTanggal"] == '' || $_GET["StartTan
                             FROM tpb_header AS hdr
                             LEFT JOIN plb_header AS plb ON hdr.NOMOR_DAFTAR=plb.NOMOR_DAFTAR
                             LEFT OUTER JOIN tpb_barang AS brg ON hdr.ID=brg.ID_HEADER
-                            ORDER BY SUBSTR(hdr.NOMOR_AJU,13,8) ASC, brg.ID ASC", 0);
+                            ORDER BY SUBSTR(hdr.NOMOR_AJU,13,8) ASC, brg.ID ASC LIMIT 100", 0);
 } else {
     $dataGet = $db->query("SELECT plb.NOMOR_BC11 AS PLB_NOMOR_BC11,plb.TANGGAL_BC11 AS PLB_TANGGAL_BC11,hdr.NOMOR_BC11,hdr.TANGGAL_BC11,hdr.NAMA_PEMASOK,
                             brg.ID,brg.KODE_BARANG,brg.URAIAN,brg.KODE_SATUAN,brg.JUMLAH_SATUAN,hdr.KODE_VALUTA,brg.CIF,hdr.ID_PENERIMA_BARANG,hdr.KODE_DOKUMEN_PABEAN,hdr.TANGGAL_TTD,SUBSTR(hdr.NOMOR_AJU,13,8) AS TGL_AJU,hdr.NOMOR_AJU,brg.POS_TARIF
                             FROM tpb_header AS hdr
                             LEFT JOIN plb_header AS plb ON hdr.NOMOR_DAFTAR=plb.NOMOR_DAFTAR
                             LEFT OUTER JOIN tpb_barang AS brg ON hdr.ID=brg.ID_HEADER
-                            WHERE SUBSTR(hdr.NOMOR_AJU,13,8) BETWEEN " . $_GET['StartTanggal'] . " AND " . $_GET['EndTanggal'] . "
+                            -- WHERE SUBSTR(hdr.NOMOR_AJU,13,8) BETWEEN " . $_GET['StartTanggal'] . " AND " . $_GET['EndTanggal'] . "
+                            WHERE pst.TANGGAL_BC11 BETWEEN '" . $_GET['StartTanggal'] . "' AND '" . $_GET['EndTanggal'] . "'
                             ORDER BY SUBSTR(hdr.NOMOR_AJU,13,8) ASC, brg.ID ASC", 0);
 }
 $cek = $dataGet->num_rows;
