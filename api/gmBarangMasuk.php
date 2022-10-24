a95a79d7aa4e85da2ba312799a4ab06948c244e3
muhammadamran <amranhakim9@gmail.com>
2022-10-24 23:52:46 +0700

    Initial commit

--- api/gmBarangMasuk.php
+++ api/gmBarangMasuk.php
@@ -8,8 +8,12 @@ if (function_exists($_GET['function'])) {
 function get_noAJU()
 {
     global $db;
-    $dataGet = $db->query("SELECT ID,NOMOR_AJU,SUBSTR(NOMOR_AJU,13,8) AS TGL_AJU,PEMASOK,KODE_NEGARA_PEMASOK,NOMOR_DAFTAR 
-                        FROM plb_header WHERE NOMOR_AJU LIKE '%" . $_GET['AJU_PLB'] . "%'", 0);
+    // $dataGet = $db->query("SELECT ID,NOMOR_AJU,SUBSTR(NOMOR_AJU,13,8) AS TGL_AJU,PEMASOK,KODE_NEGARA_PEMASOK,NOMOR_DAFTAR
+    //                     FROM plb_header WHERE NOMOR_AJU LIKE '%" . $_GET['AJU_PLB'] . "%'", 0);
+    $dataGet = $db->query("SELECT hdr.ID,hdr.NOMOR_AJU,hdr.SUBSTR(NOMOR_AJU,13,8) AS hdr.TGL_AJU,hdr.PEMASOK,hdr.KODE_NEGARA_PEMASOK,hdr.NOMOR_DAFTAR,hdr.PERUSAHAAN
+                        rcd.status,rcd.keterangan
+                        FROM plb_header AS hdr
+                        LEFT OUTER JOIN rcd_status AS rcd ON hdr.NOMOR_AJU WHERE hdr.NOMOR_AJU LIKE '%" . $_GET['AJU_PLB'] . "%'", 0);
     $cek = $dataGet->num_rows;
 
     if ($cek > 0) {
@@ -17,12 +21,15 @@ function get_noAJU()
 
         while ($result = $dataGet->fetch_assoc()) {
             $data[] = [
+                // HEADER
                 'ID' => $result['ID'],
                 'NOMOR_AJU' => $result['NOMOR_AJU'],
                 'TGL_AJU' => $result['TGL_AJU'],
                 'PEMASOK' => $result['PEMASOK'],
                 'KODE_NEGARA_PEMASOK' => $result['KODE_NEGARA_PEMASOK'],
-                'NOMOR_DAFTAR' => $result['NOMOR_DAFTAR']
+                'NOMOR_DAFTAR' => $result['NOMOR_DAFTAR'],
+                'PERUSAHAAN' => $result['PERUSAHAAB']
+                // STATUS
             ];
         }
 
