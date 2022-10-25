6766f3767172fba318d9220d0ffdad2784927e4e
muhammadamran <amranhakim9@gmail.com>
2022-10-26 03:39:05 +0700

    Initial commit

--- api/gmBarangKeluarProses.php
+++ api/gmBarangKeluarProses.php
@@ -249,44 +249,15 @@ function PostBarangRusakAll()
     }
 }
 
-// Add
-function PostADD()
-{
-    global $db;
-    $bm_no_aju_plb          = $_GET['bm_no_aju_plb'];
-    $bk_no_aju_sarinah      = $_GET['bk_no_aju_sarinah'];
-    $bm_tgl_masuk           = $_GET['bm_tgl_masuk'] . ' ' . date('H:m:i');
-    $bm_nama_operator       = $_GET['bm_nama_operator'];
-
-    $insert = $db->query("INSERT INTO rcd_status
-                            (rcd_id,bm_no_aju_plb,bk_no_aju_sarinah,bm_tgl_masuk,bm_nama_operator)
-                            VALUES
-                            ('','$bm_no_aju_plb','$bk_no_aju_sarinah','$bm_tgl_masuk','$bm_nama_operator')");
-
-    if ($insert) {
-        echo json_encode([
-            'status' => 200,
-        ]);
-    } else {
-        echo json_encode([
-            'status' => 404,
-        ]);
-    }
-}
-
 function PostEDIT()
 {
     global $db;
     $rcd_id                 = $_GET['rcd_id'];
-    $bm_no_aju_plb          = $_GET['bm_no_aju_plb'];
-    $bk_no_aju_sarinah      = $_GET['bk_no_aju_sarinah'];
-    $bm_tgl_masuk           = $_GET['bm_tgl_masuk'] . ' ' . date('H:m:i');
-    $bm_nama_operator       = $_GET['bm_nama_operator'];
+    $bk_tgl_keluar          = $_GET['bk_tgl_keluar'] . ' ' . date('H:m:i');
+    $bk_nama_operator       = $_GET['bk_nama_operator'];
 
-    $edit = $db->query("UPDATE rcd_status SET bm_no_aju_plb='" . $bm_no_aju_plb . "',
-                                                bk_no_aju_sarinah='" . $bk_no_aju_sarinah . "',
-                                                bm_tgl_masuk='" . $bm_tgl_masuk . "',
-                                                bm_nama_operator='" . $bm_nama_operator . "'
+    $edit = $db->query("UPDATE rcd_status SET bk_tgl_keluar='" . $bk_tgl_keluar . "',
+                                                bk_nama_operator='" . $bk_nama_operator . "'
                                             WHERE rcd_id='" . $rcd_id . "'");
 
     if ($edit) {
@@ -306,7 +277,7 @@ function PostUPLOAD()
     $rcd_id                 = $_GET['rcd_id'];
     $newname                = $_GET['newname'];
 
-    $edit = $db->query("UPDATE rcd_status SET upload_beritaAcara_PLB='" . $newname . "'
+    $edit = $db->query("UPDATE rcd_status SET upload_beritaAcara_GB='" . $newname . "'
                         WHERE rcd_id='" . $rcd_id . "'");
 
     if ($edit) {
