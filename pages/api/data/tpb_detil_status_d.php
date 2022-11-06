313ffe1cf3c18c99cd5ed1e44d7e992f706613f3
tpbsistem <tpbsistem@gmail.com>
2022-11-07 00:35:02 +0700

    Initial commit S

---
+++ pages/api/data/tpb_detil_status_d.php
@@ -0,0 +1,19 @@
+<?php
+if (isset($_POST['findLimit_D'])) {
+    $data = $dbmain->query("SELECT * FROM tpb_penerima ORDER BY ID DESC LIMIT " . $_POST['Limit_D'] . "", 0);
+} else {
+    $data = $dbmain->query("SELECT * FROM tpb_penerima ORDER BY ID DESC LIMIT 100", 0);
+}
+
+$no = 0;
+while ($row = $data->fetch_assoc()) {
+    $no++;
+?>
+    <tr>
+        <td width="1%" class="f-s-600 text-inverse"><?= $no ?>.</td>
+        <td style="text-align: center;"><?= $row['ID_HEADER']; ?></td>
+        <td style="text-align: center;"><?= $row['ID']; ?></td>
+        <td style="text-align: center;"><?= $row['KODE_STATUS']; ?></td>
+        <td style="text-align: center;"><?= $row['WAKTU_STATUS']; ?></td>
+    </tr>
+<?php } ?>
