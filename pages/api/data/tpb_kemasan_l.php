102e1a11e6b0080ea92487d4e87044275b0be04a
tpbsistem <tpbsistem@gmail.com>
2022-11-07 00:06:58 +0700

    Initial commit S

---
+++ pages/api/data/tpb_kemasan_l.php
@@ -0,0 +1,29 @@
+<?php
+if (isset($_POST['findLimit_L'])) {
+    $data = $db->query("SELECT * FROM tpb_kemasan ORDER BY ID DESC LIMIT " . $_POST['Limit_L'] . "", 0);
+} else {
+    $data = $db->query("SELECT * FROM tpb_kemasan ORDER BY ID DESC LIMIT 100", 0);
+}
+
+$no = 0;
+while ($row = $data->fetch_assoc()) {
+    $no++;
+?>
+    <tr>
+        <td width="1%" class="f-s-600 text-inverse"><?= $no ?>.</td>
+        <td style="text-align: center;"><?= $row['ID_HEADER']; ?></td>
+        <td style="text-align: center;"><?= $row['ID']; ?></td>
+        <td style="text-align: center;"><?= $row['JUMLAH_KEMASAN']; ?></td>
+        <td style="text-align: center;"><?= $row['KESESUAIAN_DOKUMEN']; ?></td>
+        <td style="text-align: center;"><?= $row['KETERANGAN']; ?></td>
+        <td style="text-align: center;"><?= $row['KODE_JENIS_KEMASAN']; ?></td>
+        <td style="text-align: center;"><?= $row['MERK_KEMASAN']; ?></td>
+        <td style="text-align: center;"><?= $row['NIP_GATE_IN']; ?></td>
+        <td style="text-align: center;"><?= $row['NIP_GATE_OUT']; ?></td>
+        <td style="text-align: center;"><?= $row['NO_POLISI']; ?></td>
+        <td style="text-align: center;"><?= $row['NOMOR_SEGEL']; ?></td>
+        <td style="text-align: center;"><?= $row['SERI_KEMASAN']; ?></td>
+        <td style="text-align: center;"><?= $row['WAKTU_GATE_IN']; ?></td>
+        <td style="text-align: center;"><?= $row['WAKTU_GATE_OUT']; ?></td>
+    </tr>
+<?php } ?>
---
+++ pages/api/tpb_kemasan_sync.php
@@ -0,0 +1,48 @@
+<?php
+// ---------------ROLE DATABASE-------------------------
+// $db      = LOCAL DATABASE CONNECTION
+// $dbmain  = DOMAIN DATABASE CONNECTION
+// ---------------END ROLE DATABASE---------------------
+
+// ------------------DATETIME & USERS----------------------
+// SHOW LOCAL SERVER DATETIME
+$InputDate  = date('Y-m-d h:m:i');
+// SHOW USERS
+$meOK       = $_SESSION['nama'];
+// ------------------END DATETIME & USERS------------------
+
+// CHECK DATABASE LOCAL tpb_kemasan
+// ROLE SYNC: BASE ON ID_HEADER
+// SHOW VALUE TABLE tpb_kemasan
+$dataTPB_L = $db->query("SELECT * FROM tpb_kemasan", 0);
+// LIST COUNT TABLE  tpb_kemasan
+foreach ($dataTPB_L as $row_L) {
+    if (@$row_L['ID_HEADER']) {
+        $ID_HEADER = $row_L['ID_HEADER'];
+        // CHECK DATABASE DOMAIN tpb_kemasan
+        $dataTPB_D = $dbmain->query("SELECT * FROM tpb_kemasan WHERE ID_HEADER='$ID_HEADER'");
+        $row_D = mysqli_fetch_array($dataTPB_D);
+        // IF DATA ID_HEADER ALREADY INSERT ON DATABASE DOMAIN
+        if ($row_D['ID_HEADER'] == NULL) {
+            // COUNT ROW TABLES tpb_kemasan FROM DATABASE LOCAL
+            $CountTPB_L_to_D    = $db->query("SELECT COUNT(*) total_row FROM tpb_kemasan WHERE ID_HEADER='$ID_HEADER'");
+            $row_Count_L_to_D   = $CountTPB_L_to_D->fetch_assoc();
+            $pcs                = $row_Count_L_to_D['total_row'];
+            // ROW TABLES tpb_kemasan FROM DATABASE LOCAL
+            $dataTPB_L_to_D     = $db->query("SELECT * FROM tpb_kemasan WHERE ID_HEADER='$ID_HEADER'");
+            while ($list_L_to_D = $dataTPB_L_to_D->fetch_assoc()) {
+                $query = $dbmain->query("INSERT INTO tpb_kemasan
+                                        (ID,ID_HEADER,JUMLAH_KEMASAN,KESESUAIAN_DOKUMEN,KETERANGAN,KODE_JENIS_KEMASAN,MERK_KEMASAN,NIP_GATE_IN,NIP_GATE_OUT,NO_POLISI,NOMOR_SEGEL,SERI_KEMASAN,WAKTU_GATE_IN,WAKTU_GATE_OUT)
+                                        VALUES
+                                        ('','$list_L_to_D[ID_HEADER]','$list_L_to_D[JUMLAH_KEMASAN]','$list_L_to_D[KESESUAIAN_DOKUMEN]','$list_L_to_D[KETERANGAN]','$list_L_to_D[KODE_JENIS_KEMASAN]','$list_L_to_D[MERK_KEMASAN]','$list_L_to_D[NIP_GATE_IN]','$list_L_to_D[NIP_GATE_OUT]','$list_L_to_D[NO_POLISI]','$list_L_to_D[NOMOR_SEGEL]','$list_L_to_D[SERI_KEMASAN]','$list_L_to_D[WAKTU_GATE_IN]','$list_L_to_D[WAKTU_GATE_OUT]')
+                                    ");
+            }
+        }
+        // IF ALREADY SYNC NO ACTION
+    }
+}
+if ($query) {
+    echo '<script>alert("Data has been Added");location.href = "../../index.php?m=api&s=tpb_kemasan&t=Data TPB Pungutan"</script>';
+} else {
+    echo '<script>alert("Data failed Added");location.href = "../../index.php?m=api&s=tpb_kemasan&t=Data TPB Pungutan"</script>';
+}
