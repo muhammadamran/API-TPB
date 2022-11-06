7892795089d7495f23983f9092e1103740ce30cf
amranhakim10 <amranhakim9@gmail.com>
2022-11-06 16:29:39 +0700

    Initial commit

---
+++ db/dbmain.php
@@ -0,0 +1,6 @@
+<?php
+$dbhost = '103.82.240.55';
+$dbusername = 'inxmiles_tpb';
+$dbpassword = 'Flatrone2241TPB';
+$dbname = 'inxmiles_tpb';
+$dbmain = new mysqli($dbhost, $dbusername, $dbpassword, $dbname) or die(mysqli_connect_errno());
